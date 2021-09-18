1d0bad58b86d8bbd68adb2871b57dbacf317eab6
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-09-18 02:55:44 -0400

    Cleaned up code and deleted obsolete files

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function doLogin()
 //	var tmp = {login:login,password:hash};
 	var jsonPayload = JSON.stringify( tmp );
 	
-	var url = '/login.' + extension;
+	var url = 'php/login.' + extension;
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -88,7 +88,7 @@ function doSignUp()
 //	var tmp = {login:login,password:hash};
 	var jsonPayload = JSON.stringify( tmp );
 	
-	var url = '/SignUp.' + extension;
+	var url = 'php/signUp.' + extension;
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -125,47 +125,6 @@ function doSignUp()
 	}
 
 }
-
-// function saveCookie()
-// {
-// 	var minutes = 20;
-// 	var date = new Date();
-// 	date.setTime(date.getTime()+(minutes*60*1000));	
-// 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-// }
-
-// function readCookie()
-// {
-// 	userId = -1;
-// 	var data = document.cookie;
-// 	var splits = data.split(",");
-// 	for(var i = 0; i < splits.length; i++) 
-// 	{
-// 		var thisOne = splits[i].trim();
-// 		var tokens = thisOne.split("=");
-// 		if( tokens[0] == "firstName" )
-// 		{
-// 			firstName = tokens[1];
-// 		}
-// 		else if( tokens[0] == "lastName" )
-// 		{
-// 			lastName = tokens[1];
-// 		}
-// 		else if( tokens[0] == "userId" )
-// 		{
-// 			userId = parseInt( tokens[1].trim() );
-// 		}
-// 	}
-	
-// 	if( userId < 0 )
-// 	{
-// 		window.location.href = "index.html";
-// 	}
-// 	else
-// 	{
-// 		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-// 	}
-// }
 
 function doLogout()
 {
