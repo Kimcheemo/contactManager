ce17033578c9bac3da6504d0b0698c9fc44d3490
Lezette <lezette427@gmail.com>
2021-09-19 22:13:12 -0400

    server files

--- js/homePageJS.js
+++ js/homePageJS.js
@@ -19,197 +19,6 @@ $(function () {
 
 
 
-//populateContacts();
-
- // function populateContacts(){
-//	contacts.forEach(contact => createContactCard(contact));
-// }
-
-// function PopulateList()
-// {
-
-//   // Create a request variable and assign a new XMLHttpRequest object to it.
-//   var xhr = new XMLHttpRequest();
-//   var url = "api/getAllContact.php";
-
-//   // Sending and receiving data in JSON format using POST method
-//   xhr.open("POST", url, true);
-//   xhr.setRequestHeader("Content-Type", "application/json");
-//   xhr.onload = function () {
-//       if (xhr.readyState === 4 && xhr.status === 200) {
-//           var json = JSON.parse(xhr.responseText);
-//           if(json.status != 0)
-//             {
-//                 alert(json.message);
-//                 return;
-//             }
-//           if(json.contacts)
-//           {
-//             var contacts = json.contacts;
-//             contacts.forEach( function (obj)
-//             {
-//               CreateAccordion(obj.contact_id, obj.name, obj.phone,
-//                 obj.address, obj.email, obj.website);
-//             }
-//             );
-//             createCookie("jwt", json.jwt);
-//             createCookie("expireAt", json.expireAt);
-//           }
-//           else
-//           {
-//             alert(json.message);
-//           }
-//       }
-//       else {
-//         console.log('error')
-//       }
-//   };
-//   var data = JSON.stringify({"jwt": readCookie("jwt")});
-//   xhr.send(data);
-// }
-
-// function saveCookie()
-// {
-// 	var minutes = 20;
-// 	var date = new Date();
-// 	date.setTime(date.getTime()+(minutes*60*1000));	
-// 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
-// }
-
-// function readCookie()
-// {
-// 	userId = -1;
-// 	var data = document.cookie;
-// 	var splits = data.split(",");
-// 	for(var i = 0; i < splits.length; i++) 
-// 	{
-// 		var thisOne = splits[i].trim();
-// 		var tokens = thisOne.split("=");
-// 		if( tokens[0] == "firstName" )
-// 		{
-// 			firstName = tokens[1];
-// 		}
-// 		else if( tokens[0] == "lastName" )
-// 		{
-// 			lastName = tokens[1];
-// 		}
-// 		else if( tokens[0] == "userId" )
-// 		{
-// 			userId = parseInt( tokens[1].trim() );
-// 		}
-// 	}
-	
-// 	if( userId < 0 )
-// 	{
-// 		window.location.href = "index.html";
-// 	}
-// 	else
-// 	{
-// 		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-// 	}
-// }
-
-// function getCookieData(){
-// 	userId = -1;
-// 	var data = document.cookie;
-// 	var splits = data.split(",");
-// 	for(var i = 0; i < splits.length; i++) 
-// 	{
-// 		var thisOne = splits[i].trim();
-// 		var tokens = thisOne.split("=");
-// 		if( tokens[0] == "firstName" )
-// 		{
-// 			firstName = tokens[1];
-// 		}
-// 		else if( tokens[0] == "lastName" )
-// 		{
-// 			lastName = tokens[1];
-// 		}
-// 		else if( tokens[0] == "userId" )
-// 		{
-// 			userId = parseInt( tokens[1].trim() );
-// 		}
-// 	}
-	
-// 	return userId;
-// }
-
-function createContactCard(firstName, lastName){
-	let cardContainer = document.getElementById("card-container");
-	
-	let card = document.createElement("div");
-  	card.className = "card";
-  	cardContainer.appendChild(card);
-
-	let cardBody = document.createElement("div");
-  	cardBody.className = "card-body";
-  	card.appendChild(cardBody);
-	
-	let left = document.createElement("div");
-	left.innerHTML = firstName + lastName;
-	cardBody.appendChild(left);
-
-	let img = document.createElement("img");
-	img.src = "images/icon.png";
-	img.style.width = "30px";
-	img.style.margin = "20px";
-	left.insertAdjacentElement('afterbegin', img);
-
-	let span = document.createElement("span");
-	cardBody.appendChild(span);
-
-	let dots = document.createElement("div");
-	dots.className = "dropdown";
-	span.appendChild(dots);
-
-	let anchor = document.createElement("a");
-	anchor.id = "imageDropdown";
-	anchor.setAttribute("data-toggle", "dropdown" );
-	dots.appendChild(anchor);
-
-	let imgDot = document.createElement("img");
-	imgDot.src = "images/ellipsis.png";
-	imgDot.style.width = "30px";
-	imgDot.style.cursor = "pointer";
-	anchor.appendChild(imgDot);
-
-	let dropdownlist = document.createElement("ul");
-	dropdownlist.className = "dropdown-menu";
-	dropdownlist.setAttribute("role", "menu" );
-	dropdownlist.setAttribute("aria-labelledby", "imageDropdown" );
-	dots.appendChild(dropdownlist);
-
-	let list1 = document.createElement("li");
-	list1.setAttribute("data-toggle", "modal" );
-	list1.setAttribute("data-target", "#editContact" );
-	list1.setAttribute("role", "presentation" );
-	dropdownlist.appendChild(list1);
-
-	let item1 = document.createElement("a");
-	item1.className = "ml-3";
-	item1.setAttribute("role", "menuitem" );
-	item1.setAttribute("tabindex", "-1" );
-	item1.innerText = "Edit";
-	item1.style.cursor = "pointer";
-	list1.appendChild(item1);
-
-	let list2 = document.createElement("li");
-	list2.setAttribute("data-toggle", "modal" );
-	list2.setAttribute("data-target", "#deleteContact" );
-	list2.setAttribute("role", "presentation" );
-	dropdownlist.appendChild(list2);
-
-	let item2 = document.createElement("a");
-	item2.className = "ml-3";
-	item2.setAttribute("role", "menuitem" );
-	item2.setAttribute("tabindex", "-1" );
-	item2.innerText = "Delete";
-	item2.style.color = "red";
-	item2.style.cursor = "pointer";
-	list2.appendChild(item2);
-
-}
-
 function SearchContactCard(firstName, lastName, ID){
 	let cardContainer = document.getElementById("ContactList");
 	
@@ -232,16 +41,26 @@ function SearchContactCard(firstName, lastName, ID){
 	left.insertAdjacentElement('afterbegin', img);
 
 	let span = document.createElement("span");
-	span.innerHTML = ID;
 	cardBody.appendChild(span);
 
 	let dots = document.createElement("div");
 	dots.className = "dropdown";
+	
+	let IDHolder = document.createElement("p");
+	IDHolder.innerText = ID;
+	IDHolder.className = "contactIDselect";
+	IDHolder.style.margin = "0px 0px 0px 0px";
+	IDHolder.style.fontSize = "0px";
+
+	dots.appendChild(IDHolder);
 	span.appendChild(dots);
+
+	// IDHolder.appendChild(dots);
 
 	let anchor = document.createElement("a");
 	anchor.id = "imageDropdown";
 	anchor.setAttribute("data-toggle", "dropdown" );
+	anchor.addEventListener("click", getContactID);
 	dots.appendChild(anchor);
 
 	let imgDot = document.createElement("img");
@@ -267,6 +86,8 @@ function SearchContactCard(firstName, lastName, ID){
 	item1.setAttribute("role", "menuitem" );
 	item1.setAttribute("tabindex", "-1" );
 	item1.innerText = "Edit";
+	item1.addEventListener("click", addID);
+	item1.addEventListener("click", fillContactInfo);
 	item1.style.cursor = "pointer";
 	list1.appendChild(item1);
 
@@ -280,6 +101,7 @@ function SearchContactCard(firstName, lastName, ID){
 	item2.className = "ml-3";
 	item2.setAttribute("role", "menuitem" );
 	item2.setAttribute("tabindex", "-1" );
+	item2.addEventListener("click", addID);
 	item2.innerText = "Delete";
 	item2.style.color = "red";
 	item2.style.cursor = "pointer";
@@ -287,6 +109,21 @@ function SearchContactCard(firstName, lastName, ID){
 
 }
 
+const CurrentContact = {
+	contactID:0
+};
+
+let test;
+let doof;
+
+function getContactID() {
+	test = document.getElementsByClassName("dropdown show");
+}
+
+function addID() {
+	doof = test[0].getElementsByClassName("contactIDselect");
+	CurrentContact.contactID = doof[0].innerText;
+}
 
 
 function addContact()
@@ -305,7 +142,7 @@ function addContact()
 	var tmp = {"FirstName":newFirst,"LastName":newLast,"PhoneNumber":newPhone,"Email":newEmail,"Address":newAddress,"Company":newCompany,"UserID":userID};
 	var jsonPayload = JSON.stringify( tmp );
 
-	var url = '/addContact.php';
+	var url = 'php/addContact.php';
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -317,6 +154,7 @@ function addContact()
 			if (this.readyState == 4 && this.status == 200) 
 			{
 				document.getElementById("contactAddResult").innerHTML = "Contact has been added!";
+				searchContact();
 			}
 		};
 		xhr.send(jsonPayload);
@@ -341,7 +179,7 @@ function searchContact()
 	var tmp = {"search":srch,"UserID":userId};
 	var jsonPayload = JSON.stringify( tmp );
 
-	var url = '/searchContact.php';
+	var url = 'php/searchContact.php';
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -373,11 +211,12 @@ function searchContact()
 	
 }
 
+
+
 function EditContact()
 {	
-	var userData = getCookieData();
-	userId = userData;
-
+	let contactID = CurrentContact.contactID;
+	
 	var newFirst = document.getElementById("editFirst").value;
 	var newLast = document.getElementById("editLast").value;
 	var newEmail = document.getElementById("editEmail").value;
@@ -387,10 +226,10 @@ function EditContact()
 
 	document.getElementById("contactEditResult").innerHTML = "";
 
-	var tmp = {FirstName:newFirst,LastName:newLast,PhoneNumber:newPhone,Email:newEmail,Address:newAddress,Company:newCompany,ID:userId};
+	var tmp = {FirstName:newFirst,LastName:newLast,PhoneNumber:newPhone,Email:newEmail,Address:newAddress,Company:newCompany,ID:contactID};
 	var jsonPayload = JSON.stringify( tmp );
 
-	var url = '/edit.php';
+	var url = 'php/edit.php';
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -402,6 +241,7 @@ function EditContact()
 			if (this.readyState == 4 && this.status == 200) 
 			{
 				document.getElementById("contactEditResult").innerHTML = "Contact has been edited!";
+				searchContact();
 			}
 		};
 		xhr.send(jsonPayload);
@@ -415,15 +255,14 @@ function EditContact()
 
 function deleteContact()
 {	
-	var userData = getCookieData();
-	userId = userData;
+	let contactID = CurrentContact.contactID;
 
-	var tmp = {ID:userId};
+	var tmp = {ID:contactID};
 	var jsonPayload = JSON.stringify( tmp );
 
     document.getElementById("deleteContactResult").innerHTML = "";
 
-	var url = '/delete.php';
+	var url = 'php/delete.php';
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -435,6 +274,7 @@ function deleteContact()
 			if (this.readyState == 4 && this.status == 200) 
 			{
 				document.getElementById("deleteContactResult").innerHTML = "Contact has been deleted!";
+				searchContact();
 			}
 		};
 		xhr.send(jsonPayload);
@@ -444,4 +284,41 @@ function deleteContact()
 		document.getElementById("deleteContactResult").innerHTML = "bad delete";
 	}
 	
+
+}
+
+function fillContactInfo()
+{
+		let contactID = CurrentContact.contactID;
+		var url = 'php/getContact.php';
+		
+		var tmp = {ID:contactID};
+		var jsonPayload = JSON.stringify( tmp );
+		
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+		try
+		{
+			xhr.onreadystatechange = function() 
+			{
+				if (this.readyState == 4 && this.status == 200) 
+				{
+					var jsonObject = JSON.parse( xhr.responseText );
+
+					document.getElementById('editFirst').value = jsonObject.FirstName;
+					document.getElementById('editLast').value = jsonObject.LastName;
+					document.getElementById('editCompany').value = jsonObject.Company;
+					document.getElementById('editPhone').value = jsonObject.PhoneNumber;
+					document.getElementById('editEmail').value = jsonObject.Email;
+					document.getElementById('editAddress').value = jsonObject.Address;
+				}
+			};
+			xhr.send(jsonPayload);
+		}
+		catch(err)
+		{
+			document.getElementById("contactEditResult").innerHTML = "bad edit";
+		}
+		
 }
